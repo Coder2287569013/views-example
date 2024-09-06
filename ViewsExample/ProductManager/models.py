cd1588f0f67d0f131c6dfb02cb7303a528835fe4
Coder2287569013 <yarovenkovova2008@gmail.com>
2024-09-05 22:38:09 -0230

    Add models

--- ViewsExample/ProductManager/models.py
+++ ViewsExample/ProductManager/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=16, decimal_places=2)
+
+
+class Review(models.Model):
+    product = models.ForeignKey("Product", on_delete=models.CASCADE)
+    author = models.CharField(max_length=255)
+    text = models.TextField()
+    rating = models.IntegerField()
